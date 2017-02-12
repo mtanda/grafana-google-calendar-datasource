ecd1266d8811a4534c918d7d26b35c9841c750a7
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2017-02-12 21:44:23 +0900

    remove sign in/out button

--- src/annotations_query_ctrl.js
+++ src/annotations_query_ctrl.js
@@ -2,18 +2,6 @@ export class GoogleCalendarAnnotationsQueryCtrl {
 
   constructor($scope, $injector) {
     this.scope = $scope;
-    var self=this;
-    gapi.load('client:auth2', function() {
-      gapi.auth2.init({client_id: self.datasource.clientId, scope: self.datasource.scopes});
-    });
-  }
-
-  handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-  }
-
-  handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
   }
 }
 
